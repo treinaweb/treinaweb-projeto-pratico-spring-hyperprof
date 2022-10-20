cb80d05a0d250a4106cb025072080a7906dd03da
Cleyson Lima <cleysonph@gmail.com>
2022-10-19 22:10:22 -0300

    initial commit

---
+++ src/main/java/br/com/treinaweb/hyperprof/HyperprofApplication.java
@@ -0,0 +1,13 @@
+package br.com.treinaweb.hyperprof;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HyperprofApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HyperprofApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/treinaweb/hyperprof/HyperprofApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.treinaweb.hyperprof;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HyperprofApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
